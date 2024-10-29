2d7645834f6ca9b30b23581e74638715c313abef
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-29 15:43:57 +0100

    add team obj array

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const team = [
+  {
+    fullname: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+  },
+  {
+    fullname: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+  },
+  {
+    fullname: "Giorgio Verdi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+  },
+  {
+    fullname: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martaipsum@team.com",
+  },
+  {
+    fullname: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+  },
+  {
+    fullname: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+  },
+];
